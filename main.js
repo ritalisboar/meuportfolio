af3d4f7476be46fa0d5be6d71b6974b0d8ed4be9
Rita Lisboa <cassialisboar@gmail.com>
2022-03-25 17:52:39 -0300

    atualização_1

--- main.js
+++ main.js
@@ -1,110 +1,114 @@
+/** @format */
+
 // abre e fecha o menu quando clicar nos ícones: hamburguer e x //
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+	element.addEventListener("click", function () {
+		nav.classList.toggle("show")
+	})
 }
 
 // quando clicar em um item do menu, esconder o menu //
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a")
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+	link.addEventListener("click", function () {
+		nav.classList.remove("show")
+	})
 }
 
 // mudar o header da página quando der scroll //
-const header = document.querySelector('#header')
+const header = document.querySelector("#header")
 const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-  if (window.scrollY >= navHeight) {
-    // scroll é maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    // menor que a altura do header
-    header.classList.remove('scroll')
-  }
+	if (window.scrollY >= navHeight) {
+		// scroll é maior que a altura do header
+		header.classList.add("scroll")
+	} else {
+		// menor que a altura do header
+		header.classList.remove("scroll")
+	}
 }
 
 // portfolios carousel slider swiper //
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
+const swiper = new Swiper(".swiper-container", {
+	slidesPerView: 1,
+	pagination: {
+		el: ".swiper-pagination",
+		type: "bullets",
+		clickable: true
+	},
+	mousewheel: true,
+	keyboard: true,
+	breakpoints: {
+		767: {
+			slidesPerView: 3,
+			setWrapperSize: true
+		}
+	}
 })
 
 // scrollreveal: mostrar elementos quando der scroll na página //
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true
+	origin: "top",
+	distance: "30px",
+	duration: 700,
+	reset: true
 })
 
 scrollReveal.reveal(
-  `#home .image, #home .text,
+	`#home .image, #home .text,
 #about .image, #about .text,
 #tragetória header, #tragetória .card,
 #potifolios header, #portifolios .portifolios
 #contact .text, #contact .links
 footer .brand, footer .social
 `,
-  { interval: 100 }
+	{ interval: 100 }
 )
 
 // back to top //
-const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector(".back-to-top")
 
 function backToTop() {
-  if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
+	if (window.scrollY >= 560) {
+		backToTopButton.classList.add("show")
+	} else {
+		backToTopButton.classList.remove("show")
+	}
 }
 
 // menu ativo conforme a seção visível na página //
-const sections = document.querySelectorAll('main section[id]')
+const sections = document.querySelectorAll("main section[id]")
 function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+	const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+	for (const section of sections) {
+		const sectionTop = section.offsetTop
+		const sectionHeight = section.offsetHeight
+		const sectionId = section.getAttribute("id")
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+		const checkpointStart = checkpoint >= sectionTop
+		const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
-    }
-  }
+		if (checkpointStart && checkpointEnd) {
+			document
+				.querySelector("nav ul li a[href*=" + sectionId + "]")
+				.classList.add("active")
+		} else {
+			document
+				.querySelector("nav ul li a[href*=" + sectionId + "]")
+				.classList.remove("active")
+		}
+	}
 }
 
 // When Scroll //
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection()
+window.addEventListener("scroll", function () {
+	changeHeaderWhenScroll()
+	backToTop()
+	activateMenuAtCurrentSection()
 })
